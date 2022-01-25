77ef563aa4c0e5f0b8ec7393b25e707cc054159c
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-01-25 12:18:53 -0300

    Add files via upload

---
+++ if_elif_else.py
@@ -0,0 +1,22 @@
+""""Condições"""
+
+if False:
+    print("if é falso")
+elif True:
+    print("Elif é verdadeiro")
+elif False:
+    print("Elif Falso")
+else:
+    print("Falso mais uma vez")
+
+#Nota-se que leu o verdadeiro. Se houver dois Trues, o primeiro é lido. Ex:
+
+if True:
+    print("If é Verdadeiro")
+elif False:
+    print("Elif é falso")
+elif False:
+    print("Outro Elif Falso")
+else:
+    print("Falso mais uma vez")
+
